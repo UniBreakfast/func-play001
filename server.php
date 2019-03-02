a9e05a40e284156f6d67933f5e54addae3725b69
unibreakfast <unibreakfast1@gmail.com>
2019-03-02 21:27:27 +0200

    switch

--- server.php
+++ server.php
@@ -1,6 +1,19 @@
 <?php
 
-sleep(1);
+function request() {
+  foreach (func_get_args() as $arg) $values[] = $_REQUEST[$arg];
+  return $values;
+}
+list($login, $pass) = request('login','pass');
+
+switch ($_REQUEST['task']) {
+  case 'signup': {
+    list ($login, $mail, $pass) = request('login', 'mail', 'pass');
+  }
+}
+
+
+    sleep(1);
 
 echo 'response received';
 
